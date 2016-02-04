566c4e249d04b88dd33b3be34898a09531e26641
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-02-04 17:39:19 +0100

    WIP on the structure

--- choreography/structure.go
+++ choreography/structure.go
@@ -1,40 +1,77 @@
-/*
-Olivier Wulveryck - author of Gchoreography
-Copyright (C) 2015 Olivier Wulveryck
-
-This file is part of the Gchoreography project and
-is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 package choreography
 
-import (
-	"encoding/json"
-)
-
-type Message struct {
-	ID    int
-	State int
+// A Node structure is the base structure of an execution node
+type Node struct {
+	ID         int        `json:"id",yaml:"id"`
+	Name       string     `json:"name",yaml:"name"`
+	Target     string     `json:"target",yaml:"target"`
+	Interfaces Interfaces `json:"interfaces",yaml:"interfaces"`
 }
 
-// Error is a type used when any error related to the input or node structure occurs
-type Error struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+// Interfaces is a type used for adding a special unmarshaling func
+type Interfaces map[string]Interface
+
+// We need to specialised the Unmarshaling because of the Interfaces field
+func (n *Interfaces) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var temp map[string]interface{}
+	if err := unmarshal(&temp); err != nil {
+		return err
+	}
+	for interfaceName, _ := range temp {
+		switch interfaceName {
+		case "FileEngine":
+			var temp map[string]FileEngine
+			if err := unmarshal(&temp); err != nil {
+				return err
+			}
+
+		}
+	}
+	return nil
 }
 
-func (e *Error) Error() string {
-	o, _ := json.Marshal(e)
-	return string(o)
+func (n *Node) Run() {
+
+}
+
+type Artifact string
+type Engine string
+type Input string
+type Output string
+
+type State int
+
+type Interface interface {
+	Do() chan State
+	Check() chan bool
+	GetOutput() map[string]Output
+}
+
+// FileEngine
+type FileEngine struct {
+	Artifact Artifact          `json:"artifact",yaml:"artifact"`
+	Inputs   []Input           `json:"inputs",yaml:"inputs"`
+	Outputs  map[string]Output `json:"inputs",yaml:"inputs"`
+}
+
+func (e *FileEngine) Check() chan bool {
+	c := make(chan bool)
+	go func() {
+		fileIsPresent := true
+		if fileIsPresent {
+			c <- true
+		} else {
+			c <- false
+		}
+	}()
+	return c
+}
+
+func (e *FileEngine) Do() chan State {
+	c := make(chan State)
+	return c
+}
+
+func (e *FileEngine) GetOutput() map[string]Output {
+	return map[string]Output{}
 }
--- choreography/test/main.go
+++ choreography/test/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"github.com/owulveryck/khoreia/choreography"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
 )
 
 func main() {
-	choreography.Main()
+	dec := json.NewDecoder(os.Stdin)
+	var dat interface{}
+	if err := dec.Decode(&dat); err != nil {
+		panic(err)
+	}
+	d, err := yaml.Unmarshal(&dat)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", d)
+
 }
